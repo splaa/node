94bc9f02fe4304a30591130b04739c839f36744d
Андрей Лавринюк <splaandrey@gmail.com>
2017-04-07 18:23:51 +0300

    Added copy file

---
+++ lesson3/stream.js
@@ -0,0 +1,16 @@
+var http = require('http').createServer().listen(8080);
+http.on('request', function(req, res){
+    res.writeHead(200);
+    res.write('START');
+    setTimeout(function(){
+        res.write('FINISH!');
+        res.end();
+    }, 3000);
+    req.pipe(res);
+    // req.on('data', function(msg){
+    //     console.log(msg.toString());
+    // });
+    // req.on('end',function(){
+    //     res.end();
+    // });
+});
